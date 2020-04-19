a157937438e02ff9a5ff6cabbd9286ab16f7ab69
Vallerii <62169432+valerii1260@users.noreply.github.com>
2020-04-19 15:29:16 +0300

    Add files via upload

---
+++ bot_city.py
@@ -0,0 +1,24 @@
+import telebot
+from weather import WeatherPyOwn
+
+# print(WeatherPyOwn("Харьков","UA"))
+
+bot = telebot.TeleBot("901291038:AAFRjXLTK8LW0KxyByOlRp-9dCUWJXTRmOk")
+flag_w = False
+
+
+# @bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(content_types=['text'])  # реагирует на текст
+def send_echo(message):
+    global flag_w
+    if flag_w:
+        bot.send_message(message.chat.id, WeatherPyOwn(message.text.lower(), ""))
+        flag_w = False
+    elif message.text.lower() == "погода":
+        flag_w = True
+        bot.reply_to(message, "Название города ?")
+    else:
+        bot.reply_to(message, "Неизвестная команда")
+
+
+bot.polling(none_stop=True)  # завершение бота для повторного опроса
